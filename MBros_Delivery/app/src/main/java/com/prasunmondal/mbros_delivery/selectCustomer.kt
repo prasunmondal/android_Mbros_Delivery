7555b59781885d8ccad11032ea7e26425df5698f
Prasun <pmandal@akamai.com>
2020-05-01 11:28:24 +0530

    customer selection UI

--- MBros_Delivery/app/src/main/java/com/prasunmondal/mbros_delivery/selectCustomer.kt
+++ MBros_Delivery/app/src/main/java/com/prasunmondal/mbros_delivery/selectCustomer.kt
@@ -2,11 +2,26 @@ package com.prasunmondal.mbros_delivery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Spinner
+import android.widget.Toast
 
 class selectCustomer : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_customer)
+    }
+
+    fun onClickSaveUsername(view: View) {
+        val customerSelector: Spinner = findViewById(R.id.customerSelector)
+        val customerName: String = customerSelector.getSelectedItem().toString()
+
+//        localConfigs.setValue("username", username)
+//
+//        if(isValidUserName(username)) {
+//            goToMainPage()
+//        }
+        Toast.makeText(this@selectCustomer, "Selected User: " + customerName, Toast.LENGTH_SHORT).show()
     }
 }
