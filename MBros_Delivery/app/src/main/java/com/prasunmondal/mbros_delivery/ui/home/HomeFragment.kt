bb905eebf8f06128e1fc5a53ec4eb909001723bd
Prasun <pmandal@akamai.com>
2020-05-01 21:43:26 +0530

    dynamic transaction row creation

--- MBros_Delivery/app/src/main/java/com/prasunmondal/mbros_delivery/ui/home/HomeFragment.kt
+++ MBros_Delivery/app/src/main/java/com/prasunmondal/mbros_delivery/ui/home/HomeFragment.kt
@@ -1,23 +1,15 @@
 package com.prasunmondal.mbros_delivery.ui.home
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.InputType
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.jaredrummler.materialspinner.MaterialSpinner
 import com.prasunmondal.mbros_delivery.R
-import com.prasunmondal.mbros_delivery.sessionData.AppContext
-import com.prasunmondal.mbros_delivery.sessionData.CurrentSession.Singleton.instance as currentSession
-import com.prasunmondal.mbros_delivery.sessionData.FetchedRateList.Singleton.instance as fetchedRateList
+
 
 class HomeFragment : Fragment() {
 
@@ -45,6 +37,11 @@ class HomeFragment : Fragment() {
 //            addTextBox(root)
 //        }
 
+        addTransactionRow(root)
+//        val mRlayout = root.findViewById(R.id.KGLayout) as LinearLayout
+//        var myEditText = EditText(context)
+//        myEditText.setLayoutParams(ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT))
+//        mRlayout.addView(myEditText)
 
         return root
     }
@@ -124,5 +121,24 @@ class HomeFragment : Fragment() {
 //        println("linear lay ----" + linearLayout)
 ////        linearLayout.addView(llh)
 //    }
+
+    fun addTransactionRow(root: View) {
+        addPieceRow(root)
+        addKGRow(root)
+    }
+
+    fun addKGRow(root: View) {
+        val mRlayout = root.findViewById(R.id.KGLayout) as LinearLayout
+        var myEditText = EditText(context)
+        myEditText.setLayoutParams(ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT))
+        mRlayout.addView(myEditText)
+    }
+
+    fun addPieceRow(root: View) {
+        val mRlayout = root.findViewById(R.id.PieceLayout) as LinearLayout
+        var myEditText = EditText(context)
+        myEditText.setLayoutParams(ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT))
+        mRlayout.addView(myEditText)
+    }
 
 }
