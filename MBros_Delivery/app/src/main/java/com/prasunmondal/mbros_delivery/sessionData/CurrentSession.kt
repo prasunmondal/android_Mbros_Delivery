62a15bec2f56e4d3f34241729a21064f7c2a969e
Prasun <pmandal@akamai.com>
2020-05-01 20:35:36 +0530

    getter setter for some SessionDetails

--- MBros_Delivery/app/src/main/java/com/prasunmondal/mbros_delivery/sessionData/CurrentSession.kt
+++ MBros_Delivery/app/src/main/java/com/prasunmondal/mbros_delivery/sessionData/CurrentSession.kt
@@ -7,6 +7,9 @@ class CurrentSession {
     }
 
     private lateinit var currentCustomer: String
+    private lateinit var currentCustomer_totalKG: String
+    private lateinit var currentCustomer_totalPCs: String
+
 
     fun setCurrentCustomer(value: String) {
         currentCustomer = value
@@ -15,4 +18,22 @@ class CurrentSession {
     fun getCurrentCustomer(): String {
         return currentCustomer
     }
+
+    fun setCurrentCustomer_totalKG(value: String) {
+        currentCustomer_totalKG = value
+    }
+
+    fun getCurrentCustomer_totalKG(): String {
+        return currentCustomer_totalKG
+    }
+
+    fun setCurrentCustomer_totalPCs(value: String) {
+        currentCustomer_totalPCs = value
+    }
+
+    fun getCurrentCustomer_totalPCs(): String {
+        return currentCustomer_totalPCs
+    }
+
+
 }
