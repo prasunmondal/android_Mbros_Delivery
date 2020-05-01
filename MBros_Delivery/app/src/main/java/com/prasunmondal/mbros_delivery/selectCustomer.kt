0051b50c9750782a8f3f6fefcd9b0aefbac67e83
Prasun <pmandal@akamai.com>
2020-05-01 11:20:18 +0530

    add blank user selection page

---
+++ MBros_Delivery/app/src/main/java/com/prasunmondal/mbros_delivery/selectCustomer.kt
@@ -0,0 +1,12 @@
+package com.prasunmondal.mbros_delivery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class selectCustomer : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_customer)
+    }
+}
