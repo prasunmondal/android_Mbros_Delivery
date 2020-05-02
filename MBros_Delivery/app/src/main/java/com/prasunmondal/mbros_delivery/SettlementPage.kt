37cf5a9b9e9a2b5ad4f5fc0fd4362b776c3611c2
Prasun <pmandal@akamai.com>
2020-05-02 11:54:51 +0530

    toDecimal

--- MBros_Delivery/app/src/main/java/com/prasunmondal/mbros_delivery/SettlementPage.kt
+++ MBros_Delivery/app/src/main/java/com/prasunmondal/mbros_delivery/SettlementPage.kt
@@ -31,17 +31,17 @@ class SettlementPage : AppCompatActivity() {
         var currentUser = currentSession.getCurrentCustomer()
         var totalKG = currentSession.getCurrentCustomer_totalKG().toFloat()
         var pricePerKG  = fetchedRateList.getPricePerKg(currentUser).toFloat()
-        var todaysPrice = totalKG.toFloat() * pricePerKG.toFloat()
-        var prevBalance = fetchedRateList.getPrevBal(currentUser).toFloat()
+        var todaysPrice = (totalKG.toFloat() * pricePerKG).toInt()
+        var prevBalance = fetchedRateList.getPrevBal(currentUser).toInt()
         var toPay = todaysPrice + prevBalance
         var newBalance = toPay - 0
 
-        findViewById<TextView>(R.id.kgView).text = totalKG.toString()
-        findViewById<TextView>(R.id.priceView).text = pricePerKG.toString()
-        findViewById<TextView>(R.id.todayPriceView).text = todaysPrice.toInt().toString()
-        findViewById<TextView>(R.id.prevBalanceView).text = prevBalance.toInt().toString()
-        findViewById<TextView>(R.id.toPayView).text = toPay.toInt().toString()
-        findViewById<TextView>(R.id.newBalanceView).text = newBalance.toInt().toString()
+        findViewById<TextView>(R.id.kgView).text = tryNRemoveDecimal(totalKG)
+        findViewById<TextView>(R.id.priceView).text = tryNRemoveDecimal(pricePerKG)
+        findViewById<TextView>(R.id.todayPriceView).text = todaysPrice.toString()
+        findViewById<TextView>(R.id.prevBalanceView).text = prevBalance.toString()
+        findViewById<TextView>(R.id.toPayView).text = toPay.toString()
+        findViewById<TextView>(R.id.newBalanceView).text = newBalance.toString()
 
         var paidTodayView = findViewById<EditText>(R.id.paidTodayView)
         paidTodayView.addTextChangedListener(object : TextWatcher {
@@ -63,12 +63,20 @@ class SettlementPage : AppCompatActivity() {
         })
     }
 
-    fun onChangePaidTodayView(toPay: Float) {
+    fun onChangePaidTodayView(toPay: Int) {
         var paidTodayView = findViewById<EditText>(R.id.paidTodayView)
         var paidTodayText = paidTodayView.text.toString()
-        var newBalance = 0.0F
+        var newBalance = 0
         if(paidTodayText.length>0)
-            newBalance = toPay - paidTodayText.toFloat()
-        findViewById<TextView>(R.id.newBalanceView).text = newBalance.toInt().toString()
+            newBalance = toPay - paidTodayText.toInt()
+        findViewById<TextView>(R.id.newBalanceView).text = newBalance.toString()
+    }
+
+    fun tryNRemoveDecimal(value: Float): String {
+        if(value > value.toInt()) {
+            return value.toString()
+        } else {
+            return value.toInt().toString()
+        }
     }
 }
