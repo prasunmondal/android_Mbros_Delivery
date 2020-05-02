6438ec6f66b8c706493cd1f85a077554f6a08ba9
Prasun <pmandal@akamai.com>
2020-05-02 15:05:09 +0530

    add serialNo

--- MBros_Delivery/app/src/main/java/com/prasunmondal/mbros_delivery/ui/home/HomeFragment.kt
+++ MBros_Delivery/app/src/main/java/com/prasunmondal/mbros_delivery/ui/home/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.InputType
 import android.text.TextWatcher
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -24,9 +25,9 @@ class HomeFragment : Fragment() {
     private var total_KGs = 0.0
     private var total_Pieces = 0
 
+    private var unitRowHeight = 130
+
     lateinit private var GRoot: View
-
-
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -50,6 +51,7 @@ class HomeFragment : Fragment() {
     fun addTransactionRow(root: View) {
         list_PieceFields.add(addUnitRow(root, root.findViewById(R.id.PieceLayout), InputType.TYPE_CLASS_NUMBER))
         list_KGFields.add(addUnitRow(root, root.findViewById(R.id.KGLayout), InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_DECIMAL))
+        addSerialNo(root, root.findViewById<TextView>(R.id.SerialNo))
     }
 
     fun addUnitRow(root: View, containerLayout: View, inputType: Int): EditText{
@@ -57,14 +59,27 @@ class HomeFragment : Fragment() {
         var myEditText = EditText(context)
         myEditText.setLayoutParams(ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT))
         myEditText.inputType= inputType
+        myEditText.height = unitRowHeight
         mRlayout.addView(myEditText)
         setOnChangeListener(myEditText)
         return myEditText
     }
+    fun addSerialNo(root: View, containerLayout: View) {
+        val mRlayout = containerLayout as LinearLayout
+//        val mRlayout = containerLayout as LinearLayout
+        var myTextView = TextView(context)
+        myTextView.setLayoutParams(ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT))
+        myTextView.text = list_KGFields.size.toString() + "."
+//        myTextView.setText("prasun")
+        myTextView.height = unitRowHeight
+        myTextView.gravity = Gravity.CENTER
+        myTextView.textSize = 22.0F
+        mRlayout.addView(myTextView)
+//        setOnChangeListener(myTextView)
+    }
 
     fun setOnChangeListener(editText: EditText) {
         editText.addTextChangedListener(object : TextWatcher {
-            //
             override fun afterTextChanged(s: Editable) {}
 
             override fun beforeTextChanged(
